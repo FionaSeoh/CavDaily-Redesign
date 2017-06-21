a426a3fca139bc3ff0af0e4472191500f9b463c5
chapman-monroe <schapmonroe@gmail.com>
2017-06-21 11:22:45 -0400

    made the sidebar responsive

--- cavalierdaily/functions.php
+++ cavalierdaily/functions.php
@@ -145,9 +145,17 @@ require get_template_directory() . '/inc/jetpack.php';
 /** 
  * limitin the length of an excerpt for the sidebar
  */
-
-function wpdocs_custom_excerpt_length( $length ) {
+if (condition) {
+	function wpdocs_custom_excerpt_length( $length ) {
     return 12;
+	}
+	add_filter( 'excerpt_length', 'wpdocs_custom_excerpt_length', 999 );
 }
-add_filter( 'excerpt_length', 'wpdocs_custom_excerpt_length', 999 );
+else {
+	function wpdocs_custom_excerpt_length( $length ) {
+	    return 12;
+	}
+	add_filter( 'excerpt_length', 'wpdocs_custom_excerpt_length', 999 );
+}
+
 
