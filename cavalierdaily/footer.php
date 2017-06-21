c44cc88c4db60795de2aec0d3d4cff2a88eb3201
MatthewLeeHan <mlh6fc@virginia.edu>
2017-06-21 11:26:18 -0400

    added copyright text

--- cavalierdaily/footer.php
+++ cavalierdaily/footer.php
@@ -12,18 +12,10 @@
 ?>
 
 	</div><!-- #content -->
-
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'cavalierdaily' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'cavalierdaily' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'cavalierdaily' ), 'cavalierdaily', '<a href="https://automattic.com/">Underscores.me</a>' );
-			?>
+			
+            <p class="copyright">All Content © Copyright 2017, The Cavalier Daily</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
