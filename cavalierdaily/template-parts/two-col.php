8a4a86ecc245a6a46b15cf4f31b7895ac8684bba
Fiona Seoh <fionaseoh@Fionas-MBP.lan>
2017-06-21 10:47:43 -0400

    must decide between loop in two-col or loop in index

--- cavalierdaily/template-parts/two-col.php
+++ cavalierdaily/template-parts/two-col.php
@@ -17,30 +17,31 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<div class="orange">
 	</div>
-		
+
 		<?php $args = array('posts_per_page' => 4);
-                    $loop = new WP_Query($args);
-                    while($loop->have_posts()):$loop->the_post();?>
-                        <div class="latPara">
-                            <div class="latTitle">
-                                <a title="<?php the_title(); ?>"><?php the_title(); ?></a>
-                            </div>
-                        <div class="latD-A">
-                               by <?php the_author(); ?> | <?php the_time('F j, Y'); ?>
-                        </div>
-                            <div class="latContent">
-                                <?php
-                                $threshold = 120;
-                                if (strlen(get_the_content()) >= $threshold) {
-                                    while(substr(get_the_content(), $threshold, 1) != " ")
-                                        $threshold--;
-                                }
-                                $content = substr(get_the_content(),0,$threshold)."...";
-                                echo $content; ?>
-                            </div>
-                        </div>
-                        <br>
-    	<?php endwhile; ?>
+            $loop = new WP_Query($args);
+            //while($loop->have_posts()):$loop->the_post();
+            ?>
+                <div class="">
+                    <div class="title">
+                        <a title="<?php the_title(); ?>"><?php the_title(); ?></a>
+                    </div>
+                    <div class="author-time">
+                           by <?php the_author(); ?> | <?php the_time('F j, Y'); ?>
+                    </div>
+                    <div class="content">
+                        <?php
+                        $threshold = 120;
+                        if (strlen(get_the_content()) >= $threshold) {
+                            while(substr(get_the_content(), $threshold, 1) != " ")
+                                $threshold--;
+                        }
+                        $content = substr(get_the_content(),0,$threshold)."...";
+                        echo $content; ?>
+                    </div>
+                </div>
+                <br>
+    	<?php //endwhile; ?>
 	</div><!-- .entry-content -->
 	<footer class="entry-footer">
 		<?php cavalierdaily_entry_footer(); ?>
