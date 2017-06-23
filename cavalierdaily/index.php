10ba7efa0fb30dbe999d710b87e32dabde838e1f
Fiona Seoh <fionaseoh@Fionas-MBP.lan>
2017-06-23 10:10:04 -0400

    reformatted, swapped positions of featured articles with two-col articles

--- cavalierdaily/index.php
+++ cavalierdaily/index.php
@@ -16,25 +16,47 @@ get_header(); ?>
 	<div class="main-container">
 		<div id="primary" class="content-area">
 			<main id="main" class="site-main" role="main">
-
-				<?php
-				if ( have_posts() ) :
-
-					if ( is_home() && ! is_front_page() ) : ?>
-						<header>
-							<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-						</header>
+				<div id="featured">
+	                <?php
+	                query_posts('posts_per_page=3&cat=1'); /*1, 2*/
+	                if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+	                    <div class="articleImageHomePage" style="background:url(' <?php the_post_thumbnail_url();?>');
+	                        background-size: cover;
+	                        background-position: center center;
+	                        width: 90%;
+	                        height: 380px;"
+	                        >
+	                <div class="articleTitleHomePage">
+	                    <div class="orangeBox">
+	                        <h2 class="orangeArticleTitle" style=""><a href="<?php the_permalink(); ?>"><?php the_title();/*3*/ ?></a></h2>  
+	                        <h2 class="orangeBoxArticleAuthor">Posted by: <?php the_author(); ?> | <?php the_date();?></h2>
+	                    </div>
+	                
+	                </div>
+	                    </div>
+	                <!-- <p><?php the_excerpt(); ?></p> -->
+	                <?php endwhile; ?> <?php wp_reset_query(); /*4*/ ?>
+	                </div>
 
 					<?php
+					if ( have_posts() ) :
+
+						if ( is_home() && ! is_front_page() ) : ?>
+							<header>
+								<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+							</header>
+
+						<?php
+						endif;
+
+						the_posts_navigation();
+
+					else :
+
+						get_template_part( 'template-parts/content', 'none' );
+
 					endif;
 
-					the_posts_navigation();
-
-				else :
-
-					get_template_part( 'template-parts/content', 'none' );
-
-				endif;
 			/* Start Fiona's Loop */
 				while ( have_posts() ) : the_post();
 				// <h1> Beginning of Outer Index Loop</h1>
@@ -57,27 +79,7 @@ get_header(); ?>
 				get_template_part( 'template-parts/content', 'none' );
 
 			endif; ?> -->
-                <div id="featured">
-                <?php
-                query_posts('posts_per_page=3&cat=1'); /*1, 2*/
-                if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-                    <div class="articleImageHomePage" style="background:url(' <?php the_post_thumbnail_url();?>');
-                        background-size: cover;
-                        background-position: center center;
-                        width: 90%;
-                        height: 380px;"
-                        >
-                <div class="articleTitleHomePage">
-                    <div class="orangeBox">
-                        <h2 class="orangeArticleTitle" style=""><a href="<?php the_permalink(); ?>"><?php the_title();/*3*/ ?></a></h2>  
-                        <h2 class="orangeBoxArticleAuthor">Posted by: <?php the_author(); ?> | <?php the_date();?></h2>
-                    </div>
                 
-                </div>
-                    </div>
-                <!-- <p><?php the_excerpt(); ?></p> -->
-                <?php endwhile; ?> <?php wp_reset_query(); /*4*/ ?>
-                </div>
 			</main><!-- #main -->
 		</div><!-- #primary -->
 		<div class="side-bar-container">
