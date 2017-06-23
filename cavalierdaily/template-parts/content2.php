478033092d23d55fff0a3da241340c9f16a82a2d
MatthewLeeHan <mlh6fc@virginia.edu>
2017-06-23 13:56:30 -0400

    stuck, not sure how to fix this, megan is working on it

--- cavalierdaily/template-parts/content2.php
+++ cavalierdaily/template-parts/content2.php
@@ -31,23 +31,23 @@
 	<div class="entry-content categoryFeatured">
 		<?php
         get_the_post_thumbnail();
-//			the_content( sprintf(
-//				wp_kses(
-//					/* translators: %s: Name of current post. Only visible to screen readers */
-//					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'cavalierdaily' ),
-//					array(
-//						'span' => array(
-//							'class' => array(),
-//						),
-//					)
-//				),
-//				get_the_title()
-//			) );
-//
-//			wp_link_pages( array(
-//				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'cavalierdaily' ),
-//				'after'  => '</div>',
-//			) );
+			the_content( sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'cavalierdaily' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			) );
+
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'cavalierdaily' ),
+				'after'  => '</div>',
+			) );
 		?>
 	</div><!-- .entry-content -->
 
